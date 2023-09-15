504bea68e902d28a87995547cdda30f48ea736ae
Andre Kradolfer <narfdre@gmail.com>
2023-09-15 14:03:38 -0600

    adding web test to design patterns

---
+++ scores/DesignPatternDetector/web.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, jsonify, request
+from web_app import get_github
+
+app = Flask(__name__)
+
+@app.route("/design-patterns-api/health", methods=["GET"])
+def health_check():
+    return jsonify({"msg": "GH Design Patterns API"})
+
+@app.route("/design-patterns-api/calculate", methods=["GET"])
+def get_design_patterns():
+    args = request.args
+    github_repo = args['github_repo']
+    dev_key = args.get('gh_key', '')
+    branch = args.get('branch', 'main')
+    print(args)
+    return get_github(github_repo, dev_key, branch)
+
+if __name__ == "__main__":
+    debug = os.environ.get("DEBUG", False)
+    port = os.environ.get("PORT", 5050)
+    app.run(host="0.0.0.0", port=port, debug=debug)
